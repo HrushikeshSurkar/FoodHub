84d1eeac57f54e4dfc0787c4b7dc8a7f4ebba5b9
Hrushikesh <hrushikeshsurkar@gmial.com>
2024-08-20 13:30:46 +0530

    deploying to versal

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,11 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import sessionConfig from "./config/sessionConfig.js";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./api/auth/index.js"; // Adjust the import path
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -43,4 +42,5 @@ app.use((req, res) => {
   res.status(404).json({ message: `Resource not found ${req.originalUrl}` });
 });
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export the app for Vercel to use
+export default app;
